d77684a99abf3fd0878b0bcdf67a2760a39fc251
shanti singh <shantisingh22@navgurukul.org>
2023-10-25 16:54:31 +0530

    some changes

--- Header.js
+++ Header.js
@@ -1,17 +1,17 @@
 const Header=()=> {
   return (
     <>
-        <div className="header">
-            <div className="header_image">
-                <img className="img_1" src="iconlogo.jpg"/> 
-                <div className="col2">Move Hub</div>
-                <div className="col4">Add Movie</div>
-                <div className="col_3">Watch</div>
-            </div>
-        </div> 
-        <div className="header2">
-        <div className="component2">Watch</div>
-        </div>
+      <div className="header">
+          <div className="header_image">
+          	<img className="img_1" src="iconlogo.jpg"/> 
+            <div className="col2">Move Hub</div>
+            <div className="col4">Add Movie</div>
+            <div className="col_3">Watch</div>
+          </div>
+      </div> 
+      <div className="header2">
+      <div className="component2">Watch</div>
+      </div>
     </>
   );
 };
